555d0b05ceb528ddece3d3a3f9b8257badc3f392
Mayur borgude <72288591+Mayborg121@users.noreply.github.com>
2024-10-19 21:08:35 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Dice rolling game
+
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResults = document.getElementById("diceResults");
+    const diceImg = document.getElementById("diceImg");
+    const values = [];
+    const images = [];
+
+    for(let i=0 ; i < numOfDice ; i++){
+        const value = Math.floor(Math.random()*6) +1 ;
+        values.push(value);
+        images.push(`<img src = img/${value}.png alt ="Dice ${value}">`);
+    }
+
+    diceImg.innerHTML = images.join("");
+    diceResults.textContent = `Dice : ${values.join(" • ")}`;
+    
+}
+
+rollDice();
